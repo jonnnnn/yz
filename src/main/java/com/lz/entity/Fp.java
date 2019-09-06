d71a0e09e4a0410af55d780fc3f7a4098fc7b767
jonnnnn <120814065@qq.com>
2019-09-06 21:50:28 +0800

    1

---
+++ src/main/java/com/lz/entity/Fp.java
@@ -0,0 +1,277 @@
+package com.lz.entity;
+
+/**
+ * Created by z on 2018/8/13.
+ */
+public class Fp {
+   private String pk;
+   private String fk;
+   private String lineid;
+   private String createtime;
+   private String lastmodifytime;
+   private String branchid;
+   private String note;
+   private String billid;
+   private String billingdate;
+   private String deleteflag;
+   private String opname;
+   private String opid;
+   private String custid;
+   private String custno;
+   private String custname;
+   private String amount;
+   private String completeamount;
+   private String remarks;
+   private String version;
+   private String deptid;
+   private String deptname;
+   private String billstate;
+   private String typestamps;
+   private String oldinvoiceno;
+   private String taxrate;
+   private String billtype;
+   private String billrules;
+   private String workflowstate;
+   private String workflowguid;
+   private String autoorder;
+
+    public String getPk() {
+        return pk;
+    }
+
+    public void setPk(String pk) {
+        this.pk = pk;
+    }
+
+    public String getFk() {
+        return fk;
+    }
+
+    public void setFk(String fk) {
+        this.fk = fk;
+    }
+
+    public String getLineid() {
+        return lineid;
+    }
+
+    public void setLineid(String lineid) {
+        this.lineid = lineid;
+    }
+
+    public String getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(String createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getLastmodifytime() {
+        return lastmodifytime;
+    }
+
+    public void setLastmodifytime(String lastmodifytime) {
+        this.lastmodifytime = lastmodifytime;
+    }
+
+    public String getBranchid() {
+        return branchid;
+    }
+
+    public void setBranchid(String branchid) {
+        this.branchid = branchid;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getBillid() {
+        return billid;
+    }
+
+    public void setBillid(String billid) {
+        this.billid = billid;
+    }
+
+    public String getBillingdate() {
+        return billingdate;
+    }
+
+    public void setBillingdate(String billingdate) {
+        this.billingdate = billingdate;
+    }
+
+    public String getDeleteflag() {
+        return deleteflag;
+    }
+
+    public void setDeleteflag(String deleteflag) {
+        this.deleteflag = deleteflag;
+    }
+
+    public String getOpname() {
+        return opname;
+    }
+
+    public void setOpname(String opname) {
+        this.opname = opname;
+    }
+
+    public String getOpid() {
+        return opid;
+    }
+
+    public void setOpid(String opid) {
+        this.opid = opid;
+    }
+
+    public String getCustid() {
+        return custid;
+    }
+
+    public void setCustid(String custid) {
+        this.custid = custid;
+    }
+
+    public String getCustno() {
+        return custno;
+    }
+
+    public void setCustno(String custno) {
+        this.custno = custno;
+    }
+
+    public String getCustname() {
+        return custname;
+    }
+
+    public void setCustname(String custname) {
+        this.custname = custname;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getCompleteamount() {
+        return completeamount;
+    }
+
+    public void setCompleteamount(String completeamount) {
+        this.completeamount = completeamount;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getDeptid() {
+        return deptid;
+    }
+
+    public void setDeptid(String deptid) {
+        this.deptid = deptid;
+    }
+
+    public String getDeptname() {
+        return deptname;
+    }
+
+    public void setDeptname(String deptname) {
+        this.deptname = deptname;
+    }
+
+    public String getBillstate() {
+        return billstate;
+    }
+
+    public void setBillstate(String billstate) {
+        this.billstate = billstate;
+    }
+
+    public String getTypestamps() {
+        return typestamps;
+    }
+
+    public void setTypestamps(String typestamps) {
+        this.typestamps = typestamps;
+    }
+
+    public String getOldinvoiceno() {
+        return oldinvoiceno;
+    }
+
+    public void setOldinvoiceno(String oldinvoiceno) {
+        this.oldinvoiceno = oldinvoiceno;
+    }
+
+    public String getTaxrate() {
+        return taxrate;
+    }
+
+    public void setTaxrate(String taxrate) {
+        this.taxrate = taxrate;
+    }
+
+    public String getBilltype() {
+        return billtype;
+    }
+
+    public void setBilltype(String billtype) {
+        this.billtype = billtype;
+    }
+
+    public String getBillrules() {
+        return billrules;
+    }
+
+    public void setBillrules(String billrules) {
+        this.billrules = billrules;
+    }
+
+    public String getWorkflowstate() {
+        return workflowstate;
+    }
+
+    public void setWorkflowstate(String workflowstate) {
+        this.workflowstate = workflowstate;
+    }
+
+    public String getWorkflowguid() {
+        return workflowguid;
+    }
+
+    public void setWorkflowguid(String workflowguid) {
+        this.workflowguid = workflowguid;
+    }
+
+    public String getAutoorder() {
+        return autoorder;
+    }
+
+    public void setAutoorder(String autoorder) {
+        this.autoorder = autoorder;
+    }
+}
